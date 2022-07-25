eea5412f92a7b8f31ec5f586e2aa12c0acfeb518
svetlana matsyevskaya <svetlanamats@mail.ru>
2022-07-26 01:15:28 +0300

    Task2 completed

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("2Hello, World!");
+﻿int number_a = 0;
+Console.Write("Enter number a: ");
+
+string userEnter_a = Console.ReadLine();
+
+number_a = int.Parse(userEnter_a);
+
+int number_b = 0;
+
+Console.Write("Enter number b: ");
+
+string userEnter_b = Console.ReadLine();
+
+number_b = int.Parse(userEnter_b);
+
+if (number_a> number_b)
+{
+    Console.WriteLine($"max = {number_a}");
+}
+else
+{
+    Console.WriteLine($"max = {number_b}");
+
+}
