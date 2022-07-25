eb7d039212952ed82507aa4d4d2ceb74ba38ea4b
svetlana matsyevskaya <svetlanamats@mail.ru>
2022-07-26 01:39:41 +0300

    Task4 completed again

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("22Hello, World!");
+﻿int number_a = 0;
+Console.Write("Enter number a: ");
+
+number_a = int.Parse(Console.ReadLine());
+
+int number_b = 0;
+
+Console.Write("Enter number b: ");
+
+number_b = int.Parse(Console.ReadLine());
+
+int number_c = 0;
+
+Console.Write("Enter number c: ");
+
+number_c = int.Parse(Console.ReadLine());
+
+if (number_a> number_b)
+{
+    
+    if (number_a>number_c)
+    
+    {
+        Console.WriteLine($"max = {number_a}");
+    }
+    
+    else
+    {
+        Console.WriteLine($"max = {number_c}");
+    }
+}
+else if (number_a< number_b)
+{
+    if (number_b>number_c)
+    {
+        Console.WriteLine($"max = {number_b}");
+    }
+    else
+    {
+        Console.WriteLine($"max = {number_c}");
+    }
+}
+
