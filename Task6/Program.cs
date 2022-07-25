6f75a7c110c58990a836ecbd603453e0375072de
svetlana matsyevskaya <svetlanamats@mail.ru>
2022-07-26 01:57:13 +0300

    Task6 completed

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("22Hello, World!");
+﻿int number = 0;
+Console.Write("Enter number: ");
+
+number = int.Parse(Console.ReadLine());
+
+
+
+if (number%2==0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
