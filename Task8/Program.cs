a35e70229f556e988bec13fe6e1104d029ca82fc
svetlana matsyevskaya <svetlanamats@mail.ru>
2022-07-26 10:49:55 +0300

    all file updated

--- Task8/Program.cs
+++ Task8/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("2Hello, World!");
+﻿int number = 0;
+Console.Write("Enter number: ");
+
+number = int.Parse(Console.ReadLine());
+
+int i = 1;
+while(i<=number)
+{
+    if(i%2==0)
+    {
+        Console.Write($"{i} ");
+    }
+    i++;
+}
+    
+    
+
+        
+    
+
+
+    
